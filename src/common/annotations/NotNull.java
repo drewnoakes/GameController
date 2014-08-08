5f09ce3a18c1147445102cab40e237a02971866e
Drew Noakes <git@drewnoakes.com>
2014-08-08 15:02:58 +0100

    Add @Nullable and @NotNull annotations.
    This supports automated value analysis in IntelliJ. The annotations are
    also a useful form of documentation, regardless of tooling.

---
+++ src/common/annotations/NotNull.java
@@ -0,0 +1,9 @@
+package common.annotations;
+
+/**
+ * Indicates a value can/may not have a null value at runtime.
+ *
+ * @author Drew Noakes https://drewnoakes.com
+ */
+public @interface NotNull
+{}
---
+++ src/common/annotations/Nullable.java
@@ -0,0 +1,9 @@
+package common.annotations;
+
+/**
+ * Indicates a value can/may have a null value at runtime.
+ *
+ * @author Drew Noakes https://drewnoakes.com
+ */
+public @interface Nullable
+{}
