5b43d68d01c684fda1c01d7375dc5210922e224a
Drew Noakes <git@drewnoakes.com>
2014-09-11 22:56:09 +0100

    Add comment to HL rules, matching equivalent comment in SPL rules.

--- src/leagues/HLKid.java
+++ src/leagues/HLKid.java
@@ -9,7 +9,7 @@ public class HLKid extends HL
 //        /** The league´s directory name with its teams and icons. */
 //        leagueDirectory = "hl_kid";
         /** How many robots are in a team. */
-        teamSize = 6;
+        teamSize = 6; // 4 players + 2 subs
         /** How many robots of each team may play at one time. */
         robotsPlaying = 4;
     }
