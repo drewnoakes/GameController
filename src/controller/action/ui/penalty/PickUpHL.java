drewnoakes/GameController

commit 6104b5a562743807ca6e7ee0bfc9cd4eb5dfb635

Make GCAction.isLegal non-abstract, with default returning true.
Can then remove nine overrides that contain no logic.

src/controller/action/ui/penalty/PickUpHL.java
@@ -21,10 +21,4 @@ public class PickUpHL extends PenaltyAction
         player.penalty = Penalty.HLPickupOrIncapable;
         Log.state(state, "Request for PickUp / Incapable Player " + state.team[side].teamColor + " " + (number+1));
     }
-
-    @Override
-    public boolean isLegal(GameState state)
-    {
-        return true;
-    }
 }

src/controller/action/GCAction.java
@@ -31,7 +31,7 @@ public abstract class GCAction implements ActionListener
     /**
      * This gets called when the button an action is added to was pushed or
      * if the action is called otherwise.
-     * 
+     *
      * The action`s perform method will not be executed right away but
      * later in the GUI`s thread.
      * 
@@ -66,13 +66,17 @@ public abstract class GCAction implements ActionListener
     {}
     
     /**
-     * Must be override to determine if the action is legal at a specific
-     * state of the game.
+     * Specifies if this action is legal at a specific state of the game.
      *
      * Actions that are not legal will not be executed by the EventHandler.
+     *
+     * By default, actions are legal unless this method is overridden.
      * 
      * @param state the current data to calculate the legality by.
      * @return This is true if the action is legal.
      */
-    public abstract boolean isLegal(GameState state);
+    public boolean isLegal(GameState state)
+    {
+        return true;
+    }
 }

src/controller/action/clock/ClockTick.java
@@ -42,12 +42,6 @@ public class ClockTick extends GCAction
         }
         state.updateCoachMessages();
     }
-    
-    @Override
-    public boolean isLegal(GameState state)
-    {
-        return true;
-    }
 
     /**
      * Gets whether the clock should be running given the current GameState.

src/controller/action/net/Manual.java
@@ -56,10 +56,4 @@ public class Manual extends GCAction
             }
         }
     }
-    
-    @Override
-    public boolean isLegal(GameState state)
-    {
-        return true;
-    }
 }

src/controller/action/net/SPLCoachMessageReceived.java
@@ -28,10 +28,4 @@ public class SPLCoachMessageReceived extends GCAction
             state.splCoachMessageQueue.add(message);
         }
     }
-
-    @Override
-    public boolean isLegal(GameState state)
-    {
-        return true;
-    }
 }

src/controller/action/ui/CancelUndo.java
@@ -22,10 +22,4 @@ public class CancelUndo extends GCAction
     @Override
     public void perform(GameState state)
     {}
-    
-    @Override
-    public boolean isLegal(GameState state)
-    {
-        return true;
-    }
 }

src/controller/action/ui/Quit.java
@@ -25,10 +25,4 @@ public class Quit extends GCAction
             Clock.getInstance().stop();
         }
     }
-    
-    @Override
-    public boolean isLegal(GameState state)
-    {
-        return true;
-    }
 }

src/controller/action/ui/Testmode.java
@@ -23,10 +23,4 @@ public class Testmode extends GCAction
         state.testmode = !state.testmode;
         Log.toFile("Testmode = "+ state.testmode);
     }
-    
-    @Override
-    public boolean isLegal(GameState state)
-    {
-        return true;
-    }
 }

commit 42ed7e71b24b2d8c5d0f891dba2bb64eb1f58bdf

Fix bug introduced in 6104b5a related to penalty availability.
Pick up and Service penalties should always be available.

src/controller/action/ui/penalty/PickUpHL.java
@@ -21,4 +21,11 @@ public class PickUpHL extends PenaltyAction
         player.setPenalty(Penalty.HLPickupOrIncapable);
         game.pushState("Request for PickUp / Incapable Player " + team.getTeamColor() + " " + player.getUniformNumber());
     }
+
+    @Override
+    public boolean canExecute(@NotNull Game game, @NotNull ReadOnlyGameState state)
+    {
+        // Pick up is always available
+        return true;
+    }
 }

src/controller/action/ui/penalty/PickUpSPL.java
@@ -22,4 +22,11 @@ public class PickUpSPL extends PenaltyAction
         player.setPenalty(Penalty.SplRequestForPickup);
         game.pushState("Request for PickUp " + team.getTeamColor() + " " + player.getUniformNumber());
     }
+
+    @Override
+    public boolean canExecute(@NotNull Game game, @NotNull ReadOnlyGameState state)
+    {
+        // Pick up is always available
+        return true;
+    }
 }

src/controller/action/ui/penalty/Service.java
@@ -23,4 +23,11 @@ public class Service extends PenaltyAction
             game.pushState("Additional Request for Service " + team.getTeamColor() + " " + player.getUniformNumber());
         }
     }
+
+    @Override
+    public boolean canExecute(@NotNull Game game, @NotNull ReadOnlyGameState state)
+    {
+        // Service is always available
+        return true;
+    }
 }